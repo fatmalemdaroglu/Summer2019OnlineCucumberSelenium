5ce7d7139d340283fe21f15a4fa744c07872be1e
fatmalemdaroglu <fatmalemdaroglu@icloud.com>
2020-01-13 09:35:40 -0800

    "second commit"

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         features = "src/test/Resources/features",//path to features
         glue= "com/vytrack/step_definitions",//path to step definitions
         dryRun = false,
-        tags = "@login_with_role_ddt",
+        tags = "@smoke_test",
         plugin = {"html:target/default-cucumber-reports",
                   "json:target/cucumber.json",
                   "rerun:target/rerun.txt"}
